f0f8bde7e1a584b94ee84c72ac814268114e0bfe
Igor-Zhavnerchik <IgorZhavnerchik09@gmail.com>
2026-01-07 07:52:10 +0300

    fixed initial file creation logic

--- lib/core/providers/router_provider.dart
+++ lib/core/providers/router_provider.dart
@@ -1,4 +1,4 @@
-import 'package:cross_platform_project/presentation/screens/auth_screen.dart';
+import 'package:cross_platform_project/presentation/screens/auth_screen/auth_screen.dart';
 import 'package:cross_platform_project/presentation/screens/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
